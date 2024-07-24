4f208983eab400ae5fae06a4dcb469657be38b8a
ian723 <ndegwaian241@gmail.com>
2024-07-24 14:31:11 +0300

    message

--- index.js
+++ index.js
@@ -1,16 +1,85 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const Product = require("./models/product.model.js");
 const app = express();
+
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
 
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   res.send("Welcome Hello World");
 });
 
-mongoose.connect(
+// gets all products from the database
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// gets one product by id
+app.get("/api/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const products = await Product.findById(id);
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// creates products
+app.post("/api/products", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// update a product
+app.put("/api/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    const updatedProduct = await Product.findById(id);
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// delete a product
+app.delete("/api/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    res.status(200).json({ message: "Product deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// connects to the database
+mongoose
+  .connect(
     "mongodb+srv://ian:CRGW4Mhm3op66Qbs@backend.1wfoakx.mongodb.net/Node-app?retryWrites=true&w=majority&appName=Backend"
   )
   .then(() => {
@@ -19,4 +88,3 @@ mongoose.connect(
   .catch((error) => {
     console.log("Connection failed:", error);
   });
-  
